60c1522a90bd0ddb85efa4f62c7210fc1891df6f
sushanttal <sushantt@sushantt.local>
2015-06-29 16:10:51 +0530

    Speed improvement

--- src/FriendCounter.java
+++ src/FriendCounter.java
@@ -21,10 +21,24 @@ public class FriendCounter {
             System.out.println("3. output file ");
         }
 
-        Map<Integer, Integer> edgeFriends = new HashMap<Integer, Integer>();
-        Map<Integer, Integer> queryFriends = new HashMap<Integer, Integer>();
-
         Long startTime = System.currentTimeMillis();
+
+        Map<Integer, Integer> edgeFriends = new HashMap<Integer, Integer>();
+
+        FileInputStream fin1 = new FileInputStream(new File(args[1]));
+        BufferedReader queryReader = new BufferedReader(new InputStreamReader(fin1));
+        String line1 = queryReader.readLine();
+
+        while (line1 != null) {
+
+            String queryNode = line1.trim();
+            edgeFriends.put(Integer.parseInt(queryNode), 0);
+            line1 = queryReader.readLine();
+        }
+
+        fin1.close();
+        queryReader.close();
+
 
         FileChannel inChannel = new RandomAccessFile(new File(args[0]), "r").getChannel();
         ByteBuffer buffer = ByteBuffer.allocateDirect(10 * 1024 * 1024);
@@ -41,10 +55,16 @@ public class FriendCounter {
                 } else if(ch == 9 || ch == 32){
 
                     if(number != 0) {
-                        Integer autoint = edgeFriends.get(number);
-                        if (autoint == null)
-                            autoint = 0;
-                        edgeFriends.put(number, autoint + 1);
+
+                        if (edgeFriends.get(number) == null)
+                        {
+
+                        }
+                        else {
+
+                            Integer autoint = edgeFriends.get(number);
+                            edgeFriends.put(number, autoint + 1);
+                        }
                     }
                     number = 0;
                 }
@@ -58,60 +78,13 @@ public class FriendCounter {
         }
 
 
-
-        /* Slower one. *
-
-        Long startTime = System.currentTimeMillis();
-
-        FileInputStream fin = new FileInputStream(new File("/Users/sushantt/Documents/Talentica/RandD/com-orkut.ungraph.txt"));
-        //BufferedReader edgeReader = new BufferedReader(new InputStreamReader(fin));
-        Scanner edgeReader = new Scanner(fin, "UTF-8");
-        Map<String, AtomicInteger> edgeFriends = new HashMap<String, AtomicInteger>();
-        Map<String, Integer> queryFriends = new HashMap<String, Integer>();
-
-        while(edgeReader.hasNextLine()) {
-
-            String[] nodes = edgeReader.nextLine().split("\\s+");
-            if(nodes!=null && nodes.length > 1) {
-                String node = nodes[0];
-                AtomicInteger autoint = edgeFriends.get(node);
-                if(autoint == null)
-                    autoint = new AtomicInteger(0);
-                edgeFriends.put(node.trim(), new AtomicInteger(autoint.incrementAndGet()));
-            }
-        }
-
-        System.out.println("Time : " + ((System.currentTimeMillis() - startTime) / 1000));
-        edgeReader.close();
-        fin.close(); */
-
-        FileInputStream fin1 = new FileInputStream(new File(args[1]));
-        BufferedReader queryReader = new BufferedReader(new InputStreamReader(fin1));
-
         FileOutputStream fout = new FileOutputStream(new File(args[2]));
         BufferedWriter queryWriter = new BufferedWriter(new OutputStreamWriter(fout));
 
-        String line1 = queryReader.readLine();
-
-        while (line1 != null) {
-
-            String queryNode = line1.trim();
-            Integer parsedNode = Integer.parseInt(queryNode);
-            if(edgeFriends.get(parsedNode) != null) {
-                line1 = queryReader.readLine();
-                queryFriends.put(parsedNode, edgeFriends.get(parsedNode));
-                continue;
-            }
-
-            queryFriends.put(parsedNode, 0);
-
-            line1 = queryReader.readLine();
-        }
-
         int totalCount = 0;
-        for(Integer query : queryFriends.keySet()) {
+        for(Integer query : edgeFriends.keySet()) {
             totalCount++;
-            queryWriter.write(query + " : " + queryFriends.get(query) + "\n");
+            queryWriter.write(query + " : " + edgeFriends.get(query) + "\n");
         }
 
         System.out.println(totalCount);
@@ -119,10 +92,6 @@ public class FriendCounter {
         queryWriter.close();
         fout.flush();
         fout.close();
-
-        fin1.close();
-        queryReader.close();
-
 
         System.out.println("Time : " + ((System.currentTimeMillis() - startTime) / 1000));
 
